ec384df227bb02aba03cc9c69cd6ae86df77fa83
Yueyuanh <3117144922@qq.com>
2026-03-04 16:05:10 +0800

    test action_rate = -5.0

--- legged_gym/envs/pikachu/pikachu_amp_config.py
+++ legged_gym/envs/pikachu/pikachu_amp_config.py
@@ -213,7 +213,7 @@ class PikachuAMPCfg(LeggedRobotCfg):
             feet_air_time = 0.0
             collision = 0.0
             feet_stumble = 0.0
-            action_rate = -1.0  # -1.0
+            action_rate = -5.0  # -1.0
             stand_still = 0.0
             dof_pos_limits = 0.0
             action_smoothness = -0.002
